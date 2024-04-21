b9127f66f2daaff5788e5a0db81f0758c4cf360d
giannencljsn <giannejuson16@gmail.com>
2024-04-21 20:58:21 +0800

    Modified sql file

--- SQLstuff/final_answer.sql
+++ SQLstuff/final_answer.sql
@@ -5,8 +5,9 @@ SELECT books.title AS "Book Title",
 CONCAT(authors.first_name, ' ', authors.last_name) AS Author,
 purchase_date AS "Date Of Purchase"
 FROM purchases 
-RIGHT JOIN books
+INNER JOIN books
 ON purchases.book_id = books.id
 
-RIGHT JOIN authors ON books.author_id = authors.id
+INNER JOIN authors 
+ON books.author_id = authors.id
 ORDER BY purchases.purchase_date DESC LIMIT 10;
